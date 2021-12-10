d72a2b040d4b4bd610d2e8c4bb181a54479a2144
Harry Volker <57865118+htr-volker@users.noreply.github.com>
2021-12-10 14:40:00 +0000

    Adding Jenkins pipeline configuration (#32)
    * init jenkins files
    
    * adding dependency installation stage
    
    * adding jenkins to docker group
    
    * adding push stage
    
    * fixing push stage
    
    * adding deploy stage
    
    * fixing deploy stage
    
    * deploy stage copies compose file to manager
    
    * passing env vars to ssh command
    
    * fixing api endpoint for swarm
    
    * adding database uri credential to pipeline
    
    * changing background colour to plum
    
    * adding test script
    
    * test stage publishes report
    
    * fixing test script to install pytest-cov
    
    * added API unit tests
    
    * writing frontend tests
    
    * adding requests_mock dependency
    
    * completed unit tests for frontend
    
    * adding new coverage publish stage
    
    * fixing new coverage publish stage
    
    * undoing new coverage publish stage
    
    Co-authored-by: Ubuntu <harry@dev-jenkins.vwpgll4jm5nudigpyaru4mpqfc.zx.internal.cloudapp.net>

--- frontend/tests/test_unit.py
+++ frontend/tests/test_unit.py
@@ -1,92 +1,116 @@
 from flask import url_for
 from flask_testing import TestCase
-from application import app, db
-from application.models import Tasks
+from application import app
+from application.routes import backend_host
+import requests_mock
+
+test_task = {
+                "id": 1,
+                "description": "Test the frontend",
+                "completed": False
+            }
 
 class TestBase(TestCase):
 
     def create_app(self):
         # Defines the flask object's configuration for the unit tests
         app.config.update(
-            SQLALCHEMY_DATABASE_URI='sqlite:///',
             DEBUG=True,
             WTF_CSRF_ENABLED=False
         )
         return app
 
-    def setUp(self):
-        # Will be called before every test
-        db.create_all()
-        db.session.add(Tasks(description="Run unit tests"))
-        db.session.commit()
-
-    def tearDown(self):
-        # Will be called after every test
-        db.session.remove()
-        db.drop_all()
-
 class TestViews(TestBase):
     # Test whether we get a successful response from our routes
     def test_home_get(self):
-        response = self.client.get(url_for('home'))
-        self.assert200(response)
+        with requests_mock.Mocker() as m:
+            all_tasks = { "tasks": [test_task] }
+            m.get(f"http://{backend_host}/read/allTasks", json=all_tasks)
+            response = self.client.get(url_for('home'))
+            self.assert200(response)
     
     def test_create_task_get(self):
         response = self.client.get(url_for('create_task'))
         self.assert200(response)
 
-    def test_read_tasks_get(self):
-        response = self.client.get(url_for('read_tasks'))
-        self.assert200(response)
-
     def test_update_task_get(self):
-        response = self.client.get(url_for('update_task', id=1))
-        self.assert200(response)
+        with requests_mock.Mocker() as m:
+            m.get(f"http://{backend_host}/read/task/1", json=test_task)
+            response = self.client.get(url_for('update_task', id=1))
+            self.assert200(response)
 
 class TestRead(TestBase):
 
     def test_read_home_tasks(self):
-        response = self.client.get(url_for('home'))
-        self.assertIn(b"Run unit tests", response.data)
-    
-    def test_read_tasks_dictionary(self):
-        response = self.client.get(url_for('read_tasks'))
-        self.assertIn(b"Run unit tests", response.data)
+        with requests_mock.Mocker() as m:
+            all_tasks = { "tasks": [test_task] }
+            m.get(f"http://{backend_host}/read/allTasks", json=all_tasks)
+            response = self.client.get(url_for('home'))
+            self.assertIn(b"Test the frontend", response.data)
 
 class TestCreate(TestBase):
 
     def test_create_task(self):
-        response = self.client.post(
-            url_for('create_task'),
-            data={"description": "Testing create functionality"},
-            follow_redirects=True
-        )
-        self.assertIn(b"Testing create functionality", response.data)
+        with requests_mock.Mocker() as m:
+            all_tasks = { "tasks": 
+                [
+                    test_task,
+                    {
+                        "id": 2,
+                        "description": "Testing create functionality",
+                        "completed": False
+                    }
+                ] 
+            }
+            m.post(f"http://{backend_host}/create/task", text="Test response")
+            m.get(f"http://{backend_host}/read/allTasks", json=all_tasks)
+            response = self.client.post(
+                url_for('create_task'),
+                json={"description": "Testing create functionality"},
+                follow_redirects=True
+            )
+            self.assertIn(b"Testing create functionality", response.data)
     
 class TestUpdate(TestBase):
 
     def test_update_task(self):
-        response = self.client.post(
-            url_for('update_task', id=1),
-            data={"description": "Testing update functionality"},
-            follow_redirects=True
-        )
-        self.assertIn(b"Testing update functionality", response.data)
+        with requests_mock.Mocker() as m:
+            m.get(f"http://{backend_host}/read/task/1", json=test_task)
+            m.put(f"http://{backend_host}/update/task/1", text="Test response")
+            test_task["description"] = "Testing update functionality"
+            m.get(f"http://{backend_host}/read/allTasks", json={ "tasks": [test_task] })
+            response = self.client.post(
+                url_for('update_task', id=1),
+                data={"description": "Testing update functionality"},
+                follow_redirects=True
+            )
+            self.assertIn(b"Testing update functionality", response.data)
     
     def test_complete_task(self):
-        response = self.client.get(url_for('complete_task', id=1), follow_redirects=True)
-        self.assertEqual(Tasks.query.get(1).completed, True)
+        with requests_mock.Mocker() as m:
+            m.put(f"http://{backend_host}/complete/task/1")
+            test_task["completed"] = True
+            m.get(f"http://{backend_host}/read/allTasks", json={ "tasks": [test_task] })
+            response = self.client.get(url_for('complete_task', id=1), follow_redirects=True)
+            self.assert200(response)
     
     def test_incomplete_task(self):
-        response = self.client.get(url_for('incomplete_task', id=1), follow_redirects=True)
-        self.assertEqual(Tasks.query.get(1).completed, False)
+        with requests_mock.Mocker() as m:
+            m.put(f"http://{backend_host}/incomplete/task/1")
+            test_task["completed"] = False
+            m.get(f"http://{backend_host}/read/allTasks", json={ "tasks": [test_task] })
+            response = self.client.get(url_for('incomplete_task', id=1), follow_redirects=True)
+            self.assert200(response)
         
 
 class TestDelete(TestBase):
 
     def test_delete_task(self):
-        response = self.client.get(
-            url_for('delete_task', id=1),
-            follow_redirects=True
-        )
-        self.assertNotIn(b"Run unit tests", response.data)
+        with requests_mock.Mocker() as m:
+            m.delete(f"http://{backend_host}/delete/task/1")
+            m.get(f"http://{backend_host}/read/allTasks", json={ "tasks": [] })
+            response = self.client.get(
+                url_for('delete_task', id=1),
+                follow_redirects=True
+            )
+            self.assertNotIn(b"Test the frontend", response.data)
---
+++ jenkins/test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo "Test stage"
+
+# venv created, sourced
+python3 -m venv venv
+source venv/bin/activate
+
+# install pip dependencies
+pip3 install pytest pytest-cov flask_testing requests_mock
+pip3 install -r frontend/requirements.txt
+pip3 install -r backend/requirements.txt
+
+mkdir test_reports
+
+# run pytest frontend
+python3 -m pytest frontend \
+    --cov=frontend/application \
+    --cov-report term-missing \
+    --cov-report xml:test_reports/frontend_coverage.xml \
+    --junitxml=test_reports/frontend_junit_report.xml
+
+# run pytest backend
+python3 -m pytest backend \
+    --cov=backend/application \
+    --cov-report term-missing \
+    --cov-report xml:test_reports/backend_coverage.xml \
+    --junitxml=test_reports/backend_junit_report.xml
+
+# remove venv
+deactivate
+rm -rf venv
